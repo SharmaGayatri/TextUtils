2933e29d12b5978584f99daee750aabd8676b98c
Gayatri Sharma <35535876+SharmaGayatri@users.noreply.github.com>
2022-02-10 18:25:19 +0545

    Updates

---
+++ precache-manifest.4f7e756beccb6f41e46bb8b58fb3a1a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a509527e26eaed57d95eb13b5456a5",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "9ace487fd6d986bada98",
+    "url": "/TextUtils/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/TextUtils/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "e04ab51baa6a52428e47",
+    "url": "/TextUtils/static/js/3.bfe9f187.chunk.js"
+  },
+  {
+    "revision": "9ace487fd6d986bada98",
+    "url": "/TextUtils/static/js/main.f917d943.chunk.js"
+  },
+  {
+    "revision": "984665afee1365a7f9f8",
+    "url": "/TextUtils/static/js/runtime~main.f8a3def1.js"
+  }
+]);
